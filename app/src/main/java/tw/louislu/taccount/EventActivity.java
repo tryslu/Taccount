6974c5a45d6de2353c7f5242aae3b9df876b896f
LouisLu <iesukion@gmail.com>
2017-01-22 02:28:32 +0800

    Add last two Fragments. Add StatisticsFragments and SettingFragment with its layout. Add MenuItem in EventActivity.

--- app/src/main/java/tw/louislu/taccount/EventActivity.java
+++ app/src/main/java/tw/louislu/taccount/EventActivity.java
@@ -6,12 +6,19 @@ import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TabHost;
 
 import tw.louislu.taccount.Model.Event;
 
 public class EventActivity extends AppCompatActivity {
+    private static final int ZERO = 0;
     private Event _event;
-    FragmentTabHost _tabHost;
+    private FragmentTabHost _tabHost;
+    private ActionBar _actionBar;
+    private MenuItem _newExpensesMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +34,51 @@ public class EventActivity extends AppCompatActivity {
     private void initToolBar(){
         Toolbar toolBar = (Toolbar) findViewById(R.id.event_Toolbar);
         setSupportActionBar(toolBar);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setTitle(_event.getName());
+        _actionBar = getSupportActionBar();
+        _actionBar.setDisplayHomeAsUpEnabled(true);
+        _actionBar.setTitle(_event.getName());
     }
 
     //初始化及設定TabHost
     private void initTabHost(){
         _tabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
         _tabHost.setup(this, getSupportFragmentManager(), R.id.container);
-        String _expense = getString(R.string.string_tabName_expense);
+        final String _expense = getString(R.string.string_tabName_expense);
         String _wallet = getString(R.string.string_tabName_wallet);
         String _calc = getString(R.string.string_tabName_calc);
         String _setting = getString(R.string.string_tabName_setting);
         _tabHost.addTab(_tabHost.newTabSpec(_expense).setIndicator(_expense), ExpensesFragment.class, null);
         _tabHost.addTab(_tabHost.newTabSpec(_wallet).setIndicator(_wallet), WalletFragment.class, null);
+        _tabHost.addTab(_tabHost.newTabSpec(_calc).setIndicator(_calc), StatisticsFragment.class, null);
+        _tabHost.addTab(_tabHost.newTabSpec(_setting).setIndicator(_setting), SettingFragment.class, null);
+        _tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabSpec) {
+                if(tabSpec.equals(getString(R.string.string_tabName_expense))){
+                    SetMenuItemVisible(true);
+                }else if(tabSpec.equals(getString(R.string.string_tabName_wallet))){
+                    SetMenuItemVisible(false);
+                }else if(tabSpec.equals(getString(R.string.string_tabName_calc))){
+                    SetMenuItemVisible(false);
+                }else if(tabSpec.equals(getString(R.string.string_tabName_setting))){
+                    SetMenuItemVisible(false);
+                }
+            }
+        });
+    }
+
+    //設定是否可看見MenuItem
+    private void SetMenuItemVisible(boolean visible){
+        if(_newExpensesMenuItem != null) {
+            _newExpensesMenuItem.setVisible(visible);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater _inflater = getMenuInflater();
+        _inflater.inflate(R.menu.actionbar, menu);
+        _newExpensesMenuItem = menu.getItem(ZERO);
+        return true;
     }
 }
---
+++ app/src/main/java/tw/louislu/taccount/StatisticsFragment.java
@@ -0,0 +1,39 @@
+package tw.louislu.taccount;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StatisticsFragment extends Fragment {
+
+
+    public StatisticsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_statistics, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
